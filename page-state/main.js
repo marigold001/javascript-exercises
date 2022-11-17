7d2819f66751143965cd7ea402e06721f231fdbd
marigold001 <neven.josipovic.sae@gmail.com>
2022-11-17 12:43:30 +0100

    save page state with cookies by a form

--- page-state/main.js
+++ page-state/main.js
@@ -20,41 +20,118 @@ function getCookie(cname) {
   return "";
 }
 
+const form = document.querySelector("form");
 const reg = /^#([0-9A-F]{3}){1,2}$/i;
 
-const name = getCookie("name");
+// const name = getCookie("name");
 
-if(name != "") {
-    alert("Hello, welcome back " + name + ".");
-} else {
-    const namePrompt = prompt("Hi, please tell me your name.");
-    setCookie("name", namePrompt, 30);
-}
+// if(name != "") {
+//     alert("Hello, welcome back " + name + ".");
+// } else {
+//     const namePrompt = prompt("Hi, please tell me your name.");
+//     setCookie("name", namePrompt, 30);
+// }
 
-const textColor = getCookie("textColor");
-if(textColor != "") {
-    document.body.style.color = textColor;
-} else {
-    let textPrompt = prompt("Tell me the text color you would like. You must use hex code, e.g. #fff");
-    setCookie("textColor", textPrompt, 30);
-    while(reg.test(textPrompt) != true) {
-        textPrompt = prompt("Tell me the text color you would like. You must use hex code, e.g. #fff");
+// const textColor = getCookie("textColor");
+// if(textColor != "") {
+//     document.body.style.color = textColor;
+// } else {
+//     let textPrompt = prompt("Tell me the text color you would like. You must use hex code, e.g. #fff");
+//     setCookie("textColor", textPrompt, 30);
+//     while(reg.test(textPrompt) != true) {
+//         textPrompt = prompt("Tell me the text color you would like. You must use hex code, e.g. #fff");
+//     }
+//     setCookie("textColor", textPrompt, 30);
+//     document.body.style.color = textPrompt;
+// }
+
+// const backgroundColor = getCookie("backgroundColor");
+// if(backgroundColor != "") {
+//     document.body.style.backgroundColor = backgroundColor;
+// } else {
+//     let backgroundColorPrompt = prompt("Tell me the background color you would like. You must use hex code, e.g. #fff");
+//     setCookie("backgroundColor", backgroundColorPrompt, 30);
+//     while(reg.test(backgroundColorPrompt) != true) {
+//         backgroundColorPrompt = prompt("Tell me the background color you would like. You must use hex code, e.g. #fff");
+//     }
+//     setCookie("backgroundColor", backgroundColorPrompt, 30);
+//     document.body.style.backgroundColor = backgroundColorPrompt;
+// }
+
+const nameInput = form[1];
+const textColorInput = form[2];
+const backgroundColorInput = form[3];
+const button = document.createElement("button");
+const textNode = document.createTextNode("Show form");
+button.appendChild(textNode);
+
+function createButton() {
+  if (form.style.display == "") {
+    button.style.padding = "10px 20px";
+    document.body.appendChild(button);
+    button.addEventListener("click", showForm);
+    function showForm() {
+      form.style.display = "flex";
+      button.style.display = "none";
     }
-    setCookie("textColor", textPrompt, 30);
-    document.body.style.color = textPrompt;
+  }
+}
+createButton();
+
+
+let isValid = false;
+form.addEventListener("submit", saveState);
+
+function saveState(event) {
+  event.preventDefault();
+
+  if (nameInput.value != "" || nameInput.value != " ") {
+    setCookie("name", nameInput.value, 30);
+    isValid = true;
+  } else {
+    isValid = false;
+  }
+
+  if (reg.test(textColorInput.value)) {
+    setCookie("textColor", textColorInput.value, 30);
+    document.body.style.color = textColorInput.value;
+    isValid = true;
+  } else {
+    isValid = false;
+  }
+
+  if (reg.test(backgroundColorInput.value)) {
+    setCookie("backgroundColor", backgroundColorInput.value, 30);
+    document.body.style.backgroundColor = backgroundColorInput.value;
+    isValid = true;
+  } else {
+    isValid = false;
+  }
+
+  if (isValid) {
+    form.style.display = "none";
+    button.style.display = "inline-block";
+  }
 }
 
-const backgroundColor = getCookie("backgroundColor");
-if(backgroundColor != "") {
-    document.body.style.backgroundColor = backgroundColor;
-} else {
-    let backgroundColorPrompt = prompt("Tell me the background color you would like. You must use hex code, e.g. #fff");
-    setCookie("backgroundColor", backgroundColorPrompt, 30);
-    while(reg.test(backgroundColorPrompt) != true) {
-        backgroundColorPrompt = prompt("Tell me the background color you would like. You must use hex code, e.g. #fff");
-    }
-    setCookie("backgroundColor", backgroundColorPrompt, 30);
-    document.body.style.backgroundColor = backgroundColorPrompt;
+function setStyles() {
+  document.body.style.color = getCookie("textColor");
+  document.body.style.backgroundColor = getCookie("backgroundColor");
 }
 
-console.log(userName, textColor, backgroundColor);
+setStyles();
+
+function checkCookies() {
+  if (
+    getCookie("textColor") &&
+    getCookie("backgroundColor") &&
+    getCookie("name")
+  ) {
+    alert("Welcome back, " + getCookie("name") + ".");
+    form.style.display = "none";
+  }
+}
+
+
+
+checkCookies();
