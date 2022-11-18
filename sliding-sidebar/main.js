2af993a4d95df5f036a77f33c19d9711af63142c
marigold001 <neven.josipovic.sae@gmail.com>
2022-11-18 13:11:30 +0100

    added transition

--- sliding-sidebar/main.js
+++ sliding-sidebar/main.js
@@ -4,13 +4,15 @@ const visitButtons = document.querySelectorAll("nav ul li a.visit--link");
 
 listItems.forEach((item, id) => {
   item.addEventListener("mouseover", function () {
-    icons[id].style.display = "none";
+    icons[id].style.transform = "translateX(-100px)";
+    icons[id].style.width = 0;
     visitButtons[id].style.transform = "translateX(0%)";
-    visitButtons[id].style.display = "inline-block";
+    visitButtons[id].style.opacity = 1;
   });
   item.addEventListener("mouseout", function () {
-    icons[id].style.display = "inline-block";
+    icons[id].style.transform = "translate(0)";
+    icons[id].style.width = "auto";
     visitButtons[id].style.transform = "translateX(100%)";
-    visitButtons[id].style.display = "none";
+    visitButtons[id].style.opacity = 0;
   });
 });
