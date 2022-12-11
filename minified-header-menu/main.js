40ca487abcf8833ac21fb64b44cfe5f5541d24c8
marigold001 <neven.josipovic.sae@gmail.com>
2022-12-12 00:31:18 +0100

    made header menu responsive woohoo

--- minified-header-menu/main.js
+++ minified-header-menu/main.js
@@ -3,19 +3,24 @@ const menu = document.querySelector("i");
 const navigation = document.querySelector("nav.mobile");
 const logo = document.querySelector("div#header img");
 
+
 document.addEventListener("DOMContentLoaded", function() {
     window.addEventListener("scroll", function () {
         if(window.scrollY > 50) {
             header.classList.add("fixed--top");
-            document.body.style.paddingTop = navigation.offsetHeight + "px";
             menu.style.fontSize = 30 + "px";
+            menu.style.lineHeight = 70 + "px";
             logo.style.height = 50 + "px";
+            logo.style.marginTop = 10 + "px";
+            const mediaQuery = window.matchMedia('(min-width: 788px)');
+            if(mediaQuery.matches) {
+                logo.style.marginTop = 0;
+            }
         } else {
             header.classList.remove("fixed--top");
-            this.document.body.style.paddingTop = 0;
             menu.style.fontSize = 40 + "px";
+            menu.style.lineHeight = 100 + "px";
             logo.style.height = 90 + "px";
-
         }
     })
 })
@@ -24,4 +29,6 @@ menu.addEventListener("click", showNavigation);
 
 function showNavigation() {
     navigation.classList.toggle("show");
+
+    navigation.style.top = header.offsetHeight + "px";
 }
